a9355a45f5c2966905f2d396172422238eceadf0
omchannawar <premom23112005@gmail.com>
2025-11-28 18:30:27 +0530

    Inheritance

---
+++ OOPs/Inheritance/Hybrid/hybrid_inheritance.cpp
@@ -0,0 +1,30 @@
+/*
+    Topic:- Hybrid Inheritance in C++ (OOPs)
+    Deployed & Maintained by:- Om Channawar
+    Date:- Nov 2025
+
+    Definition:-
+    -----------
+
+    Example:-
+
+    Applications:-
+    -------------
+
+
+    Advantages:-
+    -----------
+
+
+    Disadvantages:-
+    --------------
+
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base Class 
